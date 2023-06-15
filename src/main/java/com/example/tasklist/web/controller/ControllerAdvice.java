090d5de1271ae2e43407f197507b3e977eccea5c
dvg4000 <11083408-dvg4000@users.noreply.gitlab.com>
2023-06-15 20:32:11 +0300

    Add ControllerAdvice.

---
+++ src/main/java/com/example/tasklist/web/controller/ControllerAdvice.java
@@ -0,0 +1,85 @@
+package com.example.tasklist.web.controller;
+
+import com.example.tasklist.domain.exception.AccessDeniedException;
+import com.example.tasklist.domain.exception.ExceptionBody;
+import com.example.tasklist.domain.exception.ResourceMappingException;
+import com.example.tasklist.domain.exception.ResourceNotFoundException;
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class ControllerAdvice {
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ExceptionBody handleResourceNotFound(ResourceNotFoundException e) {
+        return new ExceptionBody(e.getMessage());
+    }
+
+    @ExceptionHandler(ResourceMappingException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ExceptionBody handleResourceMapping(ResourceMappingException e) {
+        return new ExceptionBody(e.getMessage());
+    }
+
+    @ExceptionHandler(IllegalStateException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ExceptionBody handleIllegalState(IllegalStateException e) {
+        return new ExceptionBody(e.getMessage());
+    }
+
+    @ExceptionHandler({AccessDeniedException.class, org.springframework.security.access.AccessDeniedException.class})
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public ExceptionBody handleAccessDenied() {
+        return new ExceptionBody("Access denied.");
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ExceptionBody handleMethodArgumentNotValid(MethodArgumentNotValidException e) {
+        return new ExceptionBody(
+                "Validation failed.",
+                e.getBindingResult().getFieldErrors()
+                        .stream()
+                        .collect(Collectors.toMap(
+                                FieldError::getField,
+                                error -> Optional.ofNullable(error.getDefaultMessage()).orElse("")
+                        ))
+        );
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ExceptionBody handleConstraintViolation(ConstraintViolationException e) {
+        return new ExceptionBody(
+                "Validation failed.",
+                e.getConstraintViolations().stream()
+                        .collect(Collectors.toMap(
+                                violation -> violation.getPropertyPath().toString(),
+                                violation -> Optional.ofNullable(violation.getMessage()).orElse("")
+                        ))
+        );
+    }
+
+    @ExceptionHandler(AuthenticationException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ExceptionBody handleAuthentication(AuthenticationException e) {
+        return new ExceptionBody("Authentication failed.");
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ExceptionBody handleException(Exception e) {
+        e.printStackTrace();
+        return new ExceptionBody("Internal error.");
+    }
+}
